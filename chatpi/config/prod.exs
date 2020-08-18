f4a142984cab64753ba858343b278d87310a7be8
ArcQ <eddielaw296@gmail.com>
2020-08-18 19:00:24 -0400

    feat: update prod config

--- chatpi/config/prod.exs
+++ chatpi/config/prod.exs
@@ -3,6 +3,4 @@ use Mix.Config
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
 # import_config "prod.secret.exs"
-config :chatpi, ChatpiWeb.Endpoint,
-
 config :logger, level: :info
